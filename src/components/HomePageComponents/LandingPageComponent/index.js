7a3f3046bd39794d40c274df0b5191d2dab3acf1
avivashishta29 <avivashishta@gmail.com>
2022-10-07 22:47:55 +0530

    position sticky

--- src/components/HomePageComponents/LandingPageComponent/index.js
+++ src/components/HomePageComponents/LandingPageComponent/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./styles.css";
 
 function LandingPageComponent() {
-  return <div className="wrapper"></div>;
+  return <div className="wrapper">HI hello world</div>;
 }
 
 export default LandingPageComponent;
