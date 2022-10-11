09d6b98bf4da3a5a8399a946dbddcdddc6cf5a56
avivashishta29 <avivashishta@gmail.com>
2022-10-11 23:06:20 +0530

    grid

--- src/components/DashboardComponents/Grid/index.js
+++ src/components/DashboardComponents/Grid/index.js
@@ -1,5 +1,10 @@
 import React from "react";
 import "./styles.css";
+import TrendingUpRoundedIcon from "@mui/icons-material/TrendingUpRounded";
+import TrendingDownRoundedIcon from "@mui/icons-material/TrendingDownRounded";
+
+//todo: responsive + graphs + data points
+
 function Grid({ coin }) {
   return (
     <div className="coin-box">
@@ -9,6 +14,29 @@ function Grid({ coin }) {
           <p className="symbol">{coin.symbol}-USD</p>
           <p className="name">{coin.name}</p>
         </div>
+      </div>
+      <div className="data-div">
+        {coin.price_change_percentage_24h > 0 ? (
+          <div className="chip-flex">
+            <div
+              className="chip"
+              style={{
+                color: "var(--green)",
+                borderColor: "var(--green)",
+              }}
+            >
+              {"+" + coin.price_change_percentage_24h.toFixed(2) + " %"}
+            </div>
+            <TrendingUpRoundedIcon className="trending-icon" />
+          </div>
+        ) : (
+          <div className="chip-flex">
+            <div className="chip red">
+              {coin.price_change_percentage_24h.toFixed(2) + " %"}
+            </div>
+            <TrendingDownRoundedIcon className="trending-icon red" />
+          </div>
+        )}
       </div>
     </div>
   );
