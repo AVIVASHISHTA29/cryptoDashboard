ada3ba5f10999b558f766089bc219854e3bd78ed
avivashishta29 <avivashishta@gmail.com>
2022-10-11 22:07:45 +0530

    mui tabs

--- src/components/DashboardComponents/DashboardWrapper/index.js
+++ src/components/DashboardComponents/DashboardWrapper/index.js
@@ -1,12 +1,81 @@
-import React from "react";
+import * as React from "react";
 import "./styles.css";
+import PropTypes from "prop-types";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
 function DashboardWrapper({ data }) {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box sx={{ p: 3 }}>
+            <Typography sx={{ color: "white" }}>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+  }
+
+  TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
+  };
+
   return (
-    <div>
-      {data.map((item, i) => (
-        <p key={i}>{item.id}</p>
-      ))}
+    <div className="tabs-wrapper">
+      <Tabs
+        variant="fullWidth"
+        value={value}
+        onChange={handleChange}
+        aria-label="basic tabs example"
+      >
+        <Tab
+          label="Grid"
+          sx={{
+            color: "white",
+            width: "50vw",
+            fontSize: "1.2rem",
+            fontWeight: 600,
+            fontFamily: "Inter",
+            textTransform: "capitalize",
+          }}
+        />
+        <Tab
+          label="List"
+          sx={{
+            color: "white",
+            width: "50vw",
+            fontSize: "1.2rem",
+            fontWeight: 600,
+            fontFamily: "Inter",
+            textTransform: "capitalize",
+          }}
+        />
+      </Tabs>
+      <TabPanel value={value} index={0}>
+        grid
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        list
+      </TabPanel>
     </div>
   );
 }
---
+++ src/components/DashboardComponents/Grid/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./styles.css";
+function Grid() {
+  return <div></div>;
+}
+
+export default Grid;
