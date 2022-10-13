553b0c7391834212e8d3b6d19f92bdd7eccbce8e
avivashishta29 <avivashishta@gmail.com>
2022-10-13 23:05:56 +0530

    chart js

--- src/components/DashboardComponents/LineChart/index.js
+++ src/components/DashboardComponents/LineChart/index.js
@@ -1,6 +1,7 @@
 import React from "react";
-import "./styles.js";
+import "./styles.css";
 import { Line } from "react-chartjs-2";
+import { Chart as ChartJS } from "chart.js/auto";
 
 function LineChart({ chartData, options }) {
   return (
--- src/pages/CoinPage.js
+++ src/pages/CoinPage.js
@@ -1,17 +1,41 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
+import LineChart from "../components/DashboardComponents/LineChart";
 import Header from "../components/Header";
 import Loader from "../components/Loader";
+import { dates, prices } from "../prices";
 
 function CoinPage() {
   const [searchParams] = useSearchParams();
   console.log(searchParams);
 
   const [data, setData] = useState();
-  const [prices, setPrices] = useState();
+
+  const options = {
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+  };
+
   const [loading, setLoading] = useState(true);
   const [loadingChart, setLoadingChart] = useState(true);
+  const [chartData, setChartData] = useState({
+    labels: dates,
+    datasets: [
+      {
+        data: prices.map((item) => item[1]),
+        borderWidth: 2,
+        fill: false,
+        tension: 0.25,
+        backgroundColor: "white",
+        borderColor: "white",
+        pointRadius: 0,
+      },
+    ],
+  });
 
   useEffect(() => {
     if (searchParams) {
@@ -28,19 +52,33 @@ function CoinPage() {
     }
   }, [searchParams]);
 
-  useEffect(() => {
-    if (data) {
-      const API_URL = `https://api.coingecko.com/api/v3/coins/${data.id}/market_chart?vs_currency=usd&days=30&interval=daily`;
-      axios.get(API_URL).then((response) => {
-        if (response.data) {
-          setPrices(response.data.prices);
-          setLoadingChart(false);
-        } else {
-          console.log("Could not get prices");
-        }
-      });
-    }
-  }, [data]);
+  //   useEffect(() => {
+  //     if (data) {
+  //       const API_URL = `https://api.coingecko.com/api/v3/coins/${data.id}/market_chart?vs_currency=usd&days=30&interval=daily`;
+  //       axios.get(API_URL).then((response) => {
+  //         if (response.data) {
+  //           setPrices("prices>>>>", response.data.prices);
+  //           setLoadingChart(false);
+  //           setChartData({
+  //             labels: prices?.map((data) => data[0]),
+  //             datasets: [
+  //               {
+  //                 data: prices?.map((data) => data[1]),
+  //                 borderWidth: 2,
+  //                 fill: false,
+  //                 tension: 0.25,
+  //                 backgroundColor: "white",
+  //                 borderColor: "white",
+  //                 pointRadius: 0,
+  //               },
+  //             ],
+  //           });
+  //         } else {
+  //           console.log("Could not get prices");
+  //         }
+  //       });
+  //     }
+  //   }, [data]);
 
   return (
     <>
@@ -49,13 +87,9 @@ function CoinPage() {
       ) : (
         <>
           <Header />
-          <p>got the data</p>
-          {prices?.map((item, i) => (
-            <>
-              <p>{i}</p>
-              <p>{item[1]}</p>
-            </>
-          ))}
+          <div>
+            <LineChart chartData={chartData} options={options} />
+          </div>
         </>
       )}
     </>
