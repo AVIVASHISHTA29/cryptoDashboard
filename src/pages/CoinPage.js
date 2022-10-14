7cf5e7f8e9fa9801b9dfc85d500db8cf76cec5c5
avivashishta29 <avivashishta@gmail.com>
2022-10-14 22:47:35 +0530

    per coin page

--- src/pages/CoinPage.js
+++ src/pages/CoinPage.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
+import OutlinedButton from "../components/OutlinedButton";
 import LineChart from "../components/DashboardComponents/LineChart";
 import Header from "../components/Header";
 import Loader from "../components/Loader";
@@ -10,6 +11,7 @@ function CoinPage() {
   console.log(searchParams);
 
   const [data, setData] = useState();
+  const [dates, setDates] = useState([]);
 
   const options = {
     plugins: {
@@ -21,70 +23,99 @@ function CoinPage() {
 
   const [loading, setLoading] = useState(true);
   const [loadingChart, setLoadingChart] = useState(true);
-  const [chartData, setChartData] = useState({});
-  const [prices, setPrices] = useState([]);
-  const today = new Date();
-  const priorDate = new Date(new Date().setDate(today.getDate() - 90));
 
-  const getDateArray = (start, end) => {
-    var arr = new Array();
-    var dt = new Date(start);
-    while (dt <= end) {
-      arr.push(new Date(dt).getDate() + "/" + (new Date(dt).getUTCMonth() + 1));
-      dt.setDate(dt.getDate() + 1);
+  const [chartData, setChartData] = useState({
+    labels: [],
+    datasets: [
+      {
+        data: [],
+        borderWidth: 2,
+        fill: false,
+        tension: 0.25,
+        backgroundColor: "white",
+        borderColor: "white",
+        pointRadius: 0,
+      },
+    ],
+  });
+
+  const [prices, setPrices] = useState([]);
+
+  const today = new Date();
+  const priorDate = new Date(new Date().setDate(today.getDate() - 30));
+
+  var getDaysArray = function (starting, ending) {
+    for (
+      var a = [], d = new Date(starting);
+      d <= new Date(ending);
+      d.setDate(d.getDate() + 1)
+    ) {
+      a.push(new Date(d).getDate() + "/" + (new Date(d).getUTCMonth() + 1));
     }
-    return arr;
+    return a;
   };
 
   useEffect(() => {
     if (searchParams) {
-      const API_URL = `https://api.coingecko.com/api/v3/coins/${searchParams}`;
-      axios
-        .get(API_URL.slice(0, -1), { crossDomain: true })
-        .then((response) => {
-          if (response.data) {
-            setData(response.data);
-            const API_URL2 = `https://api.coingecko.com/api/v3/coins/${response.data.id}/market_chart?vs_currency=usd&days=90&interval=daily`;
-            axios.get(API_URL2, { crossDomain: true }).then((response2) => {
-              if (response2.data) {
-                setPrices(response2.data.prices);
-                setLoadingChart(false);
-                setLoading(false);
-                setChartData({
-                  labels: getDateArray(priorDate, today),
-                  datasets: [
-                    {
-                      data: prices?.map((data) => data[1]),
-                      borderWidth: 2,
-                      fill: false,
-                      tension: 0.25,
-                      backgroundColor: "white",
-                      borderColor: "white",
-                      pointRadius: 0,
-                    },
-                  ],
-                });
-              } else {
-                console.log("Could not get prices");
-              }
-            });
-          } else {
-            console.log("Could not get data");
-          }
-        });
+      getData();
     }
   }, [searchParams]);
 
+  const getData = async () => {
+    const API_URL = `https://api.coingecko.com/api/v3/coins/${searchParams}`;
+
+    const response_data = await axios.get(API_URL.slice(0, -1), {
+      crossDomain: true,
+    });
+
+    if (!response_data) {
+      console.log("No data");
+      return;
+    }
+    setData(response_data.data);
+
+    const API_URL2 = `https://api.coingecko.com/api/v3/coins/${response_data.data.id}/market_chart?vs_currency=usd&days=30&interval=daily`;
+
+    const prices_data = await axios.get(API_URL2, {
+      crossDomain: true,
+    });
+
+    if (!prices_data) {
+      console.log("No price data");
+      return;
+    }
+
+    setPrices(prices_data.data.prices);
+
+    var dates_2 = getDaysArray(priorDate, today);
+
+    setChartData({
+      labels: dates_2,
+      datasets: [
+        {
+          data: prices_data?.data?.prices?.map((data) => data[1]),
+          borderWidth: 2,
+          fill: false,
+          tension: 0.25,
+          backgroundColor: "white",
+          borderColor: "white",
+          pointRadius: 0,
+        },
+      ],
+    });
+
+    setLoadingChart(false);
+    setLoading(false);
+  };
+
   return (
     <>
-      {loading ? (
+      {loading && loadingChart ? (
         <Loader />
       ) : (
         <>
           <Header />
-          <div>
-            <LineChart chartData={chartData} options={options} />
-          </div>
+          <LineChart chartData={chartData} options={options} />
         </>
       )}
     </>
