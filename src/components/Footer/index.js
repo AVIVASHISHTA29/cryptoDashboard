46a7530be32bada7f719835b0a3c8f269110515d
avivashishta29 <avivashishta@gmail.com>
2022-10-19 21:11:49 +0530

    todos

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,10 +7,10 @@ function Footer() {
 export default Footer;
 
 //todo:Add footer to every page.
-//todo:Light mode dark mode.
 //todo:Mouse follower.
-//todo:Pagination + add this component to the home page.
 //todo:Home Page more components.
 //todo: graphs not only of prices but marketcap and volume as well
 //todo :grid and list hover animation
 //todo:news api for crypto prod
+//todo:Light mode dark mode.
+//todo:Pagination + add this component to the home page.
