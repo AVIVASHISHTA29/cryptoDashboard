26dcf7a9a455dcc2620dada826e7fca8e75c130e
avivashishta29 <avivashishta@gmail.com>
2022-10-20 22:41:13 +0530

    mouse 1

--- src/App.js
+++ src/App.js
@@ -5,17 +5,42 @@ import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import CoinPage from "./pages/CoinPage";
 import ComparePage from "./pages/Compare";
+import { useEffect } from "react";
 
 function App() {
+  var cursor;
+  var cursorPointer;
+
+  useEffect(() => {
+    cursor = document.getElementById("cursor");
+    document.body.addEventListener("mousemove", function (e) {
+      return (
+        (cursor.style.left = e.clientX + "px"),
+        (cursor.style.top = e.clientY + "px")
+      );
+    });
+    cursorPointer = document.getElementById("cursor-pointer");
+    document.body.addEventListener("mousemove", function (e) {
+      return (
+        (cursorPointer.style.left = e.clientX + "px"),
+        (cursorPointer.style.top = e.clientY + "px")
+      );
+    });
+  }, []);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/compare" element={<ComparePage />} />
-        <Route path="/coin" element={<CoinPage />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <div className="cursor" id="cursor"></div>
+      <div className="cursor-pointer" id="cursor-pointer"></div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/compare" element={<ComparePage />} />
+          <Route path="/coin" element={<CoinPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
