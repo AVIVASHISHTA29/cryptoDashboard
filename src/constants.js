23e42026371e5965d4e7616b20c56e2f8e1f4b9e
avivashishta29 <avivashishta@gmail.com>
2022-10-22 18:18:37 +0530

    mobile responsive

--- src/constants.js
+++ src/constants.js
@@ -5,4 +5,4 @@ export const APP_DESCRIPTION =
 export const APP_KEYWORDS =
   "CryptoTracker., crypto, tracker, crypto dashboard, dashboard";
 export const APP_AUTHOR = "Avi Vashishta";
-export const APP_URL = "https://cryoto-tracker.netlify.app/";
+export const APP_URL = "https://crypto-dashboard-avi.netlify.app/";
